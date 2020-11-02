fa8b9e0d13093d885efede3ef3f124c54255ecd1
Moch Alfian Ferdiansyah <53443450+cyclops77@users.noreply.github.com>
2020-11-02 12:11:22 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ void main() { var app = new MyApp(); runApp(app); }
 
 class MyApp extends StatelessWidget {
    @override Widget build(BuildContext context) { 
-     return MaterialApp
-     ( title: 'sdgs 14', home: Scaffold
-     ( appBar: AppBar
-     ( title: Text('DAX'), ), 
-     body: Text('Haris Arifin - 182410103028 , Ferdy Dwi Kurniawan - 182410103090') ), ); 
+     return MaterialApp( 
+        title: 'sdgs 14',
+        home: Scaffold(
+           appBar: AppBar( 
+             title: Text('DAX'),
+           ), 
+         body: Text('Haris Arifin - 182410103028 , Ferdy Dwi Kurniawan - 182410103090') 
+        ),
+      ); 
     }
 }
