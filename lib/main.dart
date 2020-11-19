3de9a3f05d9c17068d747c4a756c03b7b97f6ef9
hris1 <55434522+hris1@users.noreply.github.com>
2020-11-19 16:42:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tasek/home/halaman-depan.dart';
+import 'package:tasek/home/start.dart';
 
-void main() { var app = new MyApp(); runApp(app); }
+void main() {
+  var app = new MyApp();
+  runApp(app);
+}
 
 class MyApp extends StatelessWidget {
-   @override Widget build(BuildContext context) { 
-     return MaterialApp
-     ( title: 'sdgs 14', home: Scaffold
-     ( appBar: AppBar
-     ( title: Text('DAX'), ), 
-     body: Text('Haris Arifin - 182410103028 , Ferdy Dwi Kurniawan - 182410103090') ), ); 
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Start(),
+    );
+  }
 }
