9121ea31f0d7de416a04f99a0d8224c88f900474
hris1 <55434522+hris1@users.noreply.github.com>
2020-12-20 08:13:09 +0700

    Update start.dart

--- lib/home/start.dart
+++ lib/home/start.dart
@@ -1,84 +1,65 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:tasek/home/home.dart';
+import 'package:tasek/layout/home.dart';
 
-class Start extends StatelessWidget {
+class Start extends StatefulWidget {
+  @override
+  _StartState createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'TASEK',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-        ),
-        backgroundColor: Color(0xFFF1C40F),
-      ),
       body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("img/bagg.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Center(
-              child: Container(
+            Image.asset("img/logo.png", width: 200, height: 300),
+            SizedBox(height: 70),
+            Text(
+              "TaSek",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 70,
+                fontFamily: "Almondita",
+              ),
+            ),
+            Text(
+              '"Selamat Datang"',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(height: 70),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 70),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                );
+              },
+              child: Image.asset(
+                "img/start.png",
                 width: 200,
                 height: 200,
-                child: new Image.asset(
-                  'img/inka.png',
-                ),
-              ),
-            ),
-            Text(
-              'Selamat Datang',
-              style: TextStyle(
-                color: Colors.amberAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 30.0,
-              ),
-            ),
-            Text(
-              'Artikel Nelayan Lokal Indonesia',
-              style: TextStyle(fontSize: 12.0),
-            ),
-            Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 400.0),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      RaisedButton(
-                        elevation: 10,
-                        color: Colors.amberAccent,
-                        child: Text(
-                          'Delok',
-                          style: TextStyle(
-                            color: Colors.blueGrey,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15.0,
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Home(),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ],
               ),
             ),
           ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.cyan,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(100.0),
-            bottomRight: Radius.circular(100.0),
-          ),
         ),
       ),
     );
